aeda547e0ea44fdbaa9907061bc938c6562540da
Samidh Desai <samidhdesai@gmail.com>
2019-02-14 13:41:43 -0600

    adds heroku database connection

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
-module.exports = new Sequelize('codegig', 'postgres', 'sspdj030394', {
+module.exports = new Sequelize('dbhahubdlnqapf', 'vkobjtnizdhnqc', '3b79819083c17155094217b78fd0822aa99cb21c61e17cb2979cc0d48522a31e', {
   
-    host: 'localhost',
+  host: 'ec2-54-227-246-152.compute-1.amazonaws.com',
   dialect:'postgres',
   operatorsAliases: false,
 
